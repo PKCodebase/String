48394d3993ccd65ba38dce3d750b3e79a201d439
PKCodebase <kaushikprasad1659@gmail.com>
2024-12-21 09:21:25 +0530

    ReverseString

---
+++ src/string/ReverseString.java
@@ -0,0 +1,23 @@
+package string;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length-1;
+
+        while(start<end){
+            char ch = s[start];
+            s[start] = s[end];
+            s[end] = ch;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        ReverseString solution = new ReverseString();
+        solution.reverseString(s);
+        System.out.println(s);
+    }
+}
+
