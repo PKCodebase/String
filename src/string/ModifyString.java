f546376ade6cd0be1da0bbda4a0c9370d3351b47
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-01 18:08:41 +0530

    ModifyString

---
+++ src/string/ModifyString.java
@@ -0,0 +1,22 @@
+package string;
+
+public class ModifyString {
+    public static void main (String [] args){
+        String str = "kaushik";
+
+        System.out.println(str.length());
+        char ch[]= str.toCharArray();
+
+        for(int i =0;i<ch.length;i++) {
+            System.out.print(ch[i] + " ");
+            ch[0] = 'K';
+            ch[6] = 'K';
+        }
+        System.out.print("\n");
+
+            for(int i =0;i<ch.length;i++){
+                System.out.print(ch[i]+" ");
+            }
+
+    }
+}
