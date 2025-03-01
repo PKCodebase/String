85e9dac38a5e47e8236ffbc28326b9eb80e66afa
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-01 17:06:20 +0530

    CheckEqualString

--- src/string/CheckEqualString.java
+++ src/string/CheckEqualString.java
@@ -7,13 +7,40 @@ public class CheckEqualString {
         String s2 = "kaushik";
         String s3 = new String("kaushik");
 
-        System.out.println(s1 == s2);
+
+        System.out.println(s1 == s2);  //True
         System.out.println(".............");
-        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s2)); //True
         System.out.println(".............");
-        System.out.println(s1==s3);
+        System.out.println(s1==s3);  //False
         System.out.println(".............");
-        System.out.println(s1.equals(s3));
+        System.out.println(s1.equals(s3)); //True
+        System.out.println(".............");
+
+        String s4 = "Hello";
+        String s5 = "Guys";
+
+        System.out.println(s4 == s5); //False
+        System.out.println(".............");
+
+        String s6 = "Hello Guys";
+        String s7 = s4.concat(s5);  //False
+        System.out.println(s6 == s7);
+        System.out.println(".............");
+
+        String s8 = "Hello".concat("Guys"); //False
+        System.out.println(s6 == s8);
+
+        System.out.println(".............");
+        System.out.println(s7 == s8);  //False
+
+        System.out.println(".............");
+
+
+        String s9 = "Kundan kumar";
+        String s10 = "Kundan " + "kumar";
+        System.out.println(s9==s10); //True
+
 
 
     }
