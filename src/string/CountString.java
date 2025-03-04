ff9852e04945062ec76ca2c1c7704568b596cb92
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-04 11:30:38 +0530

    ReverseWords2

---
+++ src/string/CountString.java
@@ -0,0 +1,16 @@
+package string;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CountString {
+    public static void main(String[] args) {
+        String str = "HelloWorld";
+        Map<Character, Long> freqMap = str.chars()
+                .mapToObj(c -> (char) c) // Convert int to Character
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        freqMap.forEach((ch, freq) -> System.out.print(ch + "" + freq));
+    }
+}
--- src/string/ReverseWords2.java
+++ src/string/ReverseWords2.java
@@ -1,4 +1,24 @@
 package string;
 
 public class ReverseWords2 {
+    public String reverseWords(String s) {
+        String[] arr = s.split(" ");
+        StringBuilder res = new StringBuilder();
+        for (int i = arr.length-1; i >= 0; i--) {
+            if(arr[i].length() == 0){
+                continue;
+            }if(res.length() == 0){
+                res.append(arr[i]);
+            }else{
+                res.append(" ");
+                res.append(arr[i]);
+            }
+        }
+        return res.toString();
+    }
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        ReverseWords2 solution = new ReverseWords2();
+        System.out.println(solution.reverseWords(s));
+    }
 }
