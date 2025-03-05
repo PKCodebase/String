3b7c353dd632c3ddc4d35d6a5bf13b6bc4414933
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-05 08:15:57 +0530

    FirstUniqueCharacter1

--- src/string/FirstUniqueCharacter1.java
+++ src/string/FirstUniqueCharacter1.java
@@ -1,26 +1,23 @@
 package string;
 
-public class FirstUniqueCharacter {
+public class FirstUniqueCharacter1 {
     public int firstUniqChar(String s) {
-        int[] freq = new int[26];
+        int[] freq = new int[58];
         char [] ch =  s.toCharArray();
         for (char c :ch) {
-            freq[c - 97]++;
+            freq[c - 65]++;
         }
         for (int i = 0; i < s.length(); i++) {
             char ch1 = s.charAt(i);
-            if (freq[ch1 - 97] == 1) {
+            if (freq[ch1 - 65] == 1) {
                 return i;
             }
         }
         return -1;
     }
     public static void main(String[] args) {
-        String s = "loveleetcode";
-        FirstUniqueCharacter solution = new FirstUniqueCharacter();
+        String s = "loveLEeTcode";
+        FirstUniqueCharacter1 solution = new FirstUniqueCharacter1();
         System.out.println(solution.firstUniqChar(s));
     }
 }
-
-//Time complexity:O(n)
-//Space complexity:O(1)
