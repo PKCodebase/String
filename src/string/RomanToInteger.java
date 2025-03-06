496045c4e4b73483dc76d328f4cba84bf2fd3f71
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-06 16:46:01 +0530

    IntegerToRoman

--- src/string/RomanToInteger.java
+++ src/string/RomanToInteger.java
@@ -4,8 +4,9 @@ public class RomanToInteger {
 
     public int romanToInt(String s) {
         int sum = 0;
-        int num = 0;
+        int previousNum = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
+            int num =0;
             switch (s.charAt(i)) {
                 case 'I':
                     num = 1;
@@ -29,19 +30,23 @@ public class RomanToInteger {
                     num = 1000;
                     break;
             }
-            if (4 * num < sum) {
+            if (num < previousNum) {
                 sum -= num;
             } else {
                 sum += num;
             }
+            previousNum = num;
         }
         return sum;
     }
 
     public static void main(String[] args) {
-        String s = "XXI";
+        String s = "XXVI";
         RomanToInteger solution = new RomanToInteger();
         System.out.println(solution.romanToInt(s));
 
     }
 }
+
+//Time : O(n)
+//Space: O(1)
