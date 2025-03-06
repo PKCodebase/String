adcb0a21680e60f95302a28545f369ddd1e52e2f
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-06 17:37:43 +0530

    RomanToInteger

--- src/string/IntegerToRoman.java
+++ src/string/IntegerToRoman.java
@@ -5,7 +5,7 @@ public class IntegerToRoman {
         StringBuilder res = new StringBuilder();
         String symbol[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         int val[]  = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        for(int i =0;num>0;i++){
+        for(int i =0;i<val.length;i++){
             while(num>=val[i]){
                 num -= val[i];
                 res.append(symbol[i]);
@@ -15,7 +15,7 @@ public class IntegerToRoman {
 
     }
     public static void main(String[] args) {
-        int num = 1994;
+        int num = 13;
         System.out.println(intToRoman(num));
     }
 }
