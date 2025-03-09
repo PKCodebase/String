365056edacdec5cf990849a73faca4096f138967
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-09 19:31:05 +0530

    DetectCapital

---
+++ src/string/DetectCapital.java
@@ -0,0 +1,22 @@
+package string;
+
+public class DetectCapital {
+    public boolean detectCapitalUse(String word) {
+        int n = word.length();
+        int count = 0;
+
+        for (char ch : word.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                count++;
+            }
+        }
+
+        return count == 0 || count == n ||
+                (count == 1 && Character.isUpperCase(word.charAt(0)));
+    }
+    public static void main(String[] args) {
+        String word = "USA";
+        DetectCapital solution = new DetectCapital();
+        System.out.println(solution.detectCapitalUse(word));
+    }
+}
