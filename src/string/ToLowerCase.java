15afcf45779f1fc67fb2fd1118a34185eb3ec78b
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-14 19:19:31 +0530

    Convert into lower case

--- src/string/ToLowerCase.java
+++ src/string/ToLowerCase.java
@@ -1,3 +1,17 @@
 package string;
 public class ToLowerCase {
+    public static String toLowerCase(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if (c >= 'A' && c <= 'Z') {
+                c += 32;
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String str = "HELLO";
+        System.out.println(toLowerCase(str));
+    }
 }
---
+++ src/string/ToLowerCase2.java
@@ -0,0 +1,11 @@
+package string;
+
+public class ToLowerCase2 {
+    public static String toLowerCase(String str) {
+       return str.toLowerCase();
+    }
+    public static void main(String[] args) {
+        String str = "HELLO";
+        System.out.println(toLowerCase(str));
+    }
+}
