2f504c72dc41db0a6ab9e755c1bfbb5383c254c2
PKCodebase <kaushikprasad1659@gmail.com>
2025-04-10 15:05:12 +0530

    ReversePrefix

---
+++ src/string/ReversePrefix.java
@@ -0,0 +1,16 @@
+package string;
+
+public class ReversePrefix {
+    public static void main(String[] args) {
+        System.out.println(reversePrefix("abcdefd", 'd'));
+    }
+
+    public static String reversePrefix(String word, char ch) {
+        int index = word.indexOf(ch);
+        StringBuilder sb = new StringBuilder();
+        sb.append(word.substring(0, index + 1));
+        sb.reverse();
+        sb.append(word.substring(index + 1));
+        return sb.toString();
+    }
+}
